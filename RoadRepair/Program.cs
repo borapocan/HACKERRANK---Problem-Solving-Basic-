dba8e1c8359378a8399676b7b6d9250c482a747b
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-11-22 07:12:25 +0300

    Add files via upload
    Road Repair

---
+++ RoadRepair/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace RoadRepair
+{
+    class Program
+    {
+        static long GetMinCost(List<int> crew, List<int> job)
+        {
+            Array.Sort(crew.ToArray());
+
+            Array.Sort(job.ToArray());
+
+            long ans = 0;
+
+            for (int i = 0; i < job.ToArray().Length; i++)
+            {
+                ans += Math.Abs(job[i] - crew[i]);
+            }
+
+            return ans;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[] crewArr = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x));
+
+            List<int> crewList = crewArr.ToList();
+
+            int[] jobArr = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x));
+
+            List<int> jobList = jobArr.ToList();
+
+            long res = GetMinCost(crewList, jobList);
+
+            Console.WriteLine(res);
+
+        }
+
+    }
+
+}
